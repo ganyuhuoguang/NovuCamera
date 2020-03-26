99949a0f791e1fcecabef98cbc1d1f258e731332
lxl <1151812935@qq.com>
2020-03-26 17:16:51 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include "mainwindow.h"
+#include "server.h"
+#include <QApplication>
+#include <sys/types.h>   // 提供类型 pid_t 的定义
+#include <unistd.h>
+#include <qdebug.h>
+#include <signal.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <QThread>
+#include <QDebug>
+#include <sys/wait.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+
+    return a.exec();
+}
